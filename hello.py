e90b02d78c5096afc8af5e2a02653affcdf2e94e
moyoujun <moyoujun00@qq.com>
2018-06-23 17:53:44 +0800

    day1 on Flask

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "<h1>hello world</h1>"
+    return render_template("index.html")
 
 
 @app.route('/user/<name>')
@@ -23,6 +23,16 @@ def user(name):
 def agent():
     user_agent = request.headers.get('user-Agent')
     return '<p>Your browser is %s</p>' % user_agent
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
